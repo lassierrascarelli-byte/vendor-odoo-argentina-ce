52c4b14e5e43e52493d6e51aa2f14df544ddadec
Dev Sandbox <devsandbox@MacBook-Air-de-Nicolas.local>
2026-02-25 16:23:16 -0300

    Odoo 19: remove pysimplesoap dependency (parse AFIP XML with lxml)

--- l10n_ar_afipws_fe/afip_utils.py
+++ l10n_ar_afipws_fe/afip_utils.py
@@ -1,10 +1,25 @@
-from pysimplesoap.client import SimpleXMLElement
-
-# import xml.etree.ElementTree as ET
+from lxml import etree
 
 
 def _get_response_info(xml_response):
-    return SimpleXMLElement(xml_response)
+    """
+    Parse XML response and return a callable similar to previous SimpleXMLElement usage:
+        xml("TagName") -> first text content for that tag (namespace-agnostic)
+    """
+    if isinstance(xml_response, bytes):
+        data = xml_response
+    else:
+        data = (xml_response or "").encode("utf-8", errors="ignore")
+
+    parser = etree.XMLParser(recover=True, huge_tree=True)
+    root = etree.fromstring(data, parser=parser)
+
+    def get(tag_name: str):
+        # Namespace-agnostic search by local-name()
+        res = root.xpath(f"//*[local-name()='{tag_name}']/text()")
+        return res[0] if res else None
+
+    return get
 
 
 def get_invoice_number_from_response(xml_response, afip_ws="wsfe"):
@@ -12,22 +27,9 @@ def get_invoice_number_from_response(xml_response, afip_ws="wsfe"):
         return False
     try:
         xml = _get_response_info(xml_response)
-        return int(xml("CbteDesde"))
-        # TODO por ahora usamos pysimplesoap porque es mas comodo
-        # Sino generar una estrategia recusiva para todos los tipos de WS
-        # namespaces = {
-        #     'soap': 'http://schemas.xmlsoap.org/soap/envelope/',
-        #     'a': NS[afip_ws],
-        # }
-        # root = _get_response_info(xml_response)
-        # number = root.findall('./soap:Body'
-        #                '/a:FECAESolicitarResponse'
-        #                '/a:FECAESolicitarResult'
-        #                '/a:FeDetResp'
-        #                '/a:FECAEDetResponse'
-        #                '/a:CbteDesde' , namespaces)[0].text
-        # return int(number)
-    except:
+        value = xml("CbteDesde")
+        return int(value) if value else False
+    except Exception:
         return False
 
 
